42526d34230f93e9c6c2ab6d8c9c1ae41dd8da85
Gregory <gpost774@gmail.com>
2022-12-14 18:42:25 +0300

    writed code

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,3 +1,4 @@
-﻿string username = Console.ReadLine();
-Console WriteLine("Привет, ");
-Console WriteLine(username);
+﻿Console.Write("Ведите ваше имя:");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
