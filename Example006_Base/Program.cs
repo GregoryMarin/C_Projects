b1a1bab578f7d9d31fa2778e26f49205ea2c8e04
Gregory <gpost774@gmail.com>
2022-12-15 19:12:06 +0300

    Другое решенин проедложенной программы

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 2;
+int c = 4;
+int d = 6;
+int e = 8;
+
+int max = a;
+
+if (a > max)
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
