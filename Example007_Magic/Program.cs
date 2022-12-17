61d0ede870365c0da2d4383b3c10ec2d0fce5fac
Gregory <gpost774@gmail.com>
2022-12-17 11:33:41 +0300

    Created a programm file

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+// 1. Write a programm that calculates the average of 2 numbers
+
+System.Console.WriteLine("Write a number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(number1);
