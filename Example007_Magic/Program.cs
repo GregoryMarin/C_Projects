2a76d3d54228e7f384312cf18ec04004c554d433
Gregory <gpost774@gmail.com>
2022-12-17 11:44:18 +0300

    Programm already

--- Example007_Magic/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-// 1. Write a programm that calculates the average of 2 numbers
-
-System.Console.WriteLine("Write a number: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(number1);
