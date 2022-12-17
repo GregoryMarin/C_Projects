c0e8b4c470413183beaa9ba778ecb29bcf19fb08
Gregory <gpost774@gmail.com>
2022-12-17 11:45:05 +0300

    The programm already finished

---
+++ Averageof_2_numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+// 1. Write a programm that calculates the average of 2 numbers
+
+System.Console.WriteLine("Write 2 numbers: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int summ = number1 + number2;
+int average = summ / 2;
+System.Console.WriteLine("The average of " + number1 + " and " + number2 + " is = " + average);
+System.Console.WriteLine("Well done!");
