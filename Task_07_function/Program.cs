523c59b8d5aeedfe40bc1cfc5a18b0cffee621f6
Gregory <gpost774@gmail.com>
2022-12-20 17:06:37 +0300

    Writed code

--- Task_07_function/Program.cs
+++ Task_07_function/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿/* Напишите число, которое на вход принимает 3-х значное число, а на выходе выдает последнюю цифру числа */
+
+Console.WriteLine("Введите 3-х значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine($"Последняя цифра числа {number} = {number % 10}");
+}
+else
+{
+    Console.Write("Некорректные данные");
+}
