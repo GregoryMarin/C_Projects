cb96a1fbdcc61cd06ed0f80650dbd410f1bc3ba3
Gregory <gpost774@gmail.com>
2022-12-20 15:38:50 +0300

    fixed bugs

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -4,40 +4,38 @@
 */
 
 Console.WriteLine("Write day number: ");
-int day = Convert.ToInt32(Console.ReadLine());
+string day = Console.ReadLine();
 
-if (day < 8)
+
+if (day == "1")
 {
-    if (day == 1)
-    {
-        Console.WriteLine("Monday");
-    }
-    if (day == 2)
-    {
-        Console.WriteLine("Tusday");
-    }
-    if (day == 3)
-    {
-        Console.WriteLine("Wednesday");
-    }
-    if (day == 4)
-    {
-        Console.WriteLine("Thursday");
-    }
-    if (day == 5)
-    {
-        System.Console.WriteLine("Friday");
-    }
-    if (day == 6)
-    {
-        System.Console.WriteLine("Saturday");
-    }
-    if (day == 7)
-    {
-        System.Console.WriteLine("Sunday");
-    }
+    Console.WriteLine("Monday");
+}
+if (day == "2")
+{
+    Console.WriteLine("Tusday");
+}
+if (day == "3")
+{
+    Console.WriteLine("Wednesday");
+}
+if (day == "4")
+{
+    Console.WriteLine("Thursday");
+}
+if (day == "5")
+{
+    System.Console.WriteLine("Friday");
+}
+if (day == "6")
+{
+    System.Console.WriteLine("Saturday");
+}
+if (day == "7")
+{
+    System.Console.WriteLine("Sunday");
 }
 else
 {
-    System.Console.WriteLine("Ther is no such a day");
+    System.Console.WriteLine("Warning! Incorrect data");
 }
