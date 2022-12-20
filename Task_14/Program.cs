e359dc5c13bced96cbbb48238a336939fdb47d8d
Gregory <gpost774@gmail.com>
2022-12-20 15:07:52 +0300

    Writed code

--- Task_14/Program.cs
+++ Task_14/Program.cs
@@ -17,3 +17,16 @@ bool IsSquare(int n1, int n2)
 -4, 16 -> да
 25, 5 -> да
 8,9 -> нет */
+
+Console.WriteLine("Введите первое число: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Square(numb1, numb2) ? "Yes" : "No");
+
+bool Square (int n1, int n2)
+{
+    return n1 / n2 == n2 || n2 / n1 == n1;
+}
