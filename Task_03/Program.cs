a7923ca8c973edf4c59bd0c7e763b3786477af90
Gregory <gpost774@gmail.com>
2023-01-02 03:20:45 +0300

    fixed code

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -4,38 +4,15 @@
 */
 
 Console.WriteLine("Write day number: ");
-string day = Console.ReadLine();
+int day = int.Parse(Console.ReadLine());
 
 
-if (day == "1")
-{
-    Console.WriteLine("Monday");
-}
-if (day == "2")
-{
-    Console.WriteLine("Tusday");
-}
-if (day == "3")
-{
-    Console.WriteLine("Wednesday");
-}
-if (day == "4")
-{
-    Console.WriteLine("Thursday");
-}
-if (day == "5")
-{
-    System.Console.WriteLine("Friday");
-}
-if (day == "6")
-{
-    System.Console.WriteLine("Saturday");
-}
-if (day == "7")
-{
-    System.Console.WriteLine("Sunday");
-}
-else
-{
-    System.Console.WriteLine("Warning! Incorrect data");
-}
+if (day == 1) Console.WriteLine("Monday");
+if (day == 2) Console.WriteLine("Tusday");
+if (day == 3) Console.WriteLine("Wednesday");
+if (day == 4) Console.WriteLine("Thursday");
+if (day == 5) System.Console.WriteLine("Friday");
+if (day == 6) System.Console.WriteLine("Saturday");
+if (day == 7) System.Console.WriteLine("Sunday");
+if (day > 7) System.Console.WriteLine("Warning! Incorrect data");
+if (day <= 0) System.Console.WriteLine("Warning! Number can not be < 0");
