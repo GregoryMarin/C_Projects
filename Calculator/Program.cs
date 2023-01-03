f4c8334f0e89bb281823cd594ad45a80ad805bdc
Gregory <gpost774@gmail.com>
2023-01-03 14:12:38 +0300

    ...

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number = int.Parse(Console.ReadLine());
+
+void Characters(int n1, int n2)
+{
+    System.Console.WriteLine("Введите значение: ");
+    string a = Console.ReadLine();
+    switch (a)
+    {
+        case "*":
+            int result = n1 * n2;
+            System.Console.WriteLine($"Результат умножения чисел = {result}");
+            break;
+        case "/":
+            int result1 = n1 / n2;
+            System.Console.WriteLine($"Результат деления чисел = {result1}");
+            break;
+        case "-":
+            int result2 = n1 - n2;
+            System.Console.WriteLine($"Результат вычисления чисел = {result2}");
+            break;
+        case "+":
+            int result3 = n1 + n2;
+            System.Console.WriteLine($"Результат сложения чисел = {result3}");
+            break;
+    }
+}
+
+Characters(number, number2);
