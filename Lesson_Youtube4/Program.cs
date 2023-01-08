05a52a45ff39cd8c0de38a9d2133d79a79e4c19a
Gregory <gpost774@gmail.com>
2023-01-08 18:56:13 +0300

    ...

---
+++ Lesson_Youtube4/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите высору прямоугольниика: ");
+int height = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите ширину прямоугольника: ");
+int width = Convert.ToInt32(Console.ReadLine());
+
+for (int j = 0; j < height; j++)
+{
+    for (int i = 0; i < width; i++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+    
---
+++ Lessons_YouTube5/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < number1; i++)
+{
+    Console.SetCursorPosition(number1 - i, i + 1);
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write("#");
+    }
+}
