cb2ef1bdcaa9e6f4f0021cf878bb981b1737cae4
Gregory <gpost774@gmail.com>
2023-01-10 20:10:16 +0300

    ...

--- Converter_Dollar_Rub/Program.cs
+++ Converter_Dollar_Rub/Program.cs
@@ -1,9 +1,14 @@
 ﻿// Напишите конвертер валют 
+using System.Globalization;
 
 System.Console.WriteLine("By: 1 US_Dollar = 65 Ruble");
 System.Console.WriteLine("Sell: 70 Ruble = 1 US_Dollar");
-System.Console.WriteLine("What currency you wont to exchange? (Write Dollar = 1 or Ruble = 2)");
+System.Console.WriteLine("What currency you want to exchange? (Write Dollar = 1 or Ruble = 2)");
 int currency = Convert.ToInt32(Console.ReadLine());
+NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+        {
+            NumberDecimalSeparator = ".",
+        };
 
 void Currency(int value1)
 {
@@ -14,7 +19,7 @@ void Currency(int value1)
         double dollar = Convert.ToDouble(Console.ReadLine());
         double result = dollar * 65.00;
         double result2 = Math.Round(result, 2, MidpointRounding.ToZero);
-        System.Console.WriteLine($"It will be {result2} Rubles");
+        System.Console.WriteLine($"It will be {result2} Rubles, {numberFormatInfo}");
     }
     if (value1 == 2)
     {
@@ -23,7 +28,7 @@ void Currency(int value1)
         double ruble = Convert.ToDouble(Console.ReadLine());
         double result = ruble / 70.00;
         double result2 = Math.Round(result, 2, MidpointRounding.ToZero);
-        System.Console.WriteLine($"It will be {result2} US_Dollars");
+        System.Console.WriteLine($"It will be {result2} US_Dollars {numberFormatInfo}");
     }
     if (value1 != 1 && value1 != 2)
     {
