827d4694c96c787b153e61de0cbf18832c42ef9b
Gregory <gpost774@gmail.com>
2023-01-10 20:09:23 +0300

    Created table of multiplys

---
+++ Multiply table/Program.cs
@@ -0,0 +1,15 @@
+﻿// Таблица умножения
+
+void Method()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            Console.WriteLine($"{i} x {j} = {i * j}");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Method();
